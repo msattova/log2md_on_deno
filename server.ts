6251bbd4284bf3a4d45892ba09cef378e1260291
msattova <msattova@gmail.com>
2022-03-09 21:32:15 +0900

    port番号変更

--- server.ts
+++ server.ts
@@ -2,6 +2,6 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts"
 
 import { handler } from "./client.ts"
 
-const addr = ":8080";
+const addr = ":8000";
 console.log(`Listening on http://localhost${addr}`);
 serve(handler, { addr });
